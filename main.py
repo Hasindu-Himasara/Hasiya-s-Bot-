75ae3c22acc48ec8caa362166d0761818fa72ba1
Tech With Hasiya <90503467+HasiyaTechlk@users.noreply.github.com>
2021-09-23 09:14:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, filters
+
+API_ID = 7407598 
+API_HASH = '6e6622a5e352ee0c86cc558ca1c17de3'
+BOT_TOKEN = '1993601562:AAGQDgbhPdHh5HkGzW4TLVC4gBJaJiTGmC8'
+
+bot = Client("Hasiya Bot",
+             api_hash=API_HASH,
+             api_id=API_ID,
+             bot_token=BOT_TOKEN)
+
+@bot.on_message(filters.command(commands=['hasiya']))
+async def welcome(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Hi...")
+    
+@bot.on_message(filters.command(commands=['help']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Just send me some messages and enjoy")
+    
+@bot.on_message(filters.command(commands=['bot']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Hi How are you")
+    
+    
+@bot.on_message(filters.command(commands=['You Tube']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Gaming Hasiya You Tube Channel Link.\n https://www.youtube.com/c/GamingHasiyaYouTube")
+    
+    
+@bot.on_message(filters.command(commands=['start']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="💫 Hi i am Hasindu Helper\n🔥 Bot made by Hasindu Himasara")
+    
+    
+@bot.on_message(filters.command(commands=['About']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="👨‍💻 Hasiya Helper 👨‍💻 - I am Hasindu's Helper Bot\n🇱🇰 Bot Made By Hasindu Himasara 🇱🇰")
+    
+    
+bot.run() 
